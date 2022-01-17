c0f0d530d7149beca402e04f7f757c6919b9cd0b
Matthieu Dorier <mdorier@anl.gov>
2022-01-17 11:05:40 +0000

    added proper mochi namespace

--- hepnos_wizard/specs.py
+++ hepnos_wizard/specs.py
@@ -28,11 +28,11 @@ def gen_database_spec(contains: str, index: int=0, type: str="map", config: dict
     return { "name": name, "type": type, "config": copy.deepcopy(config) }
 
 
-def gen_provider_spec(provider_id: int=0, pool=bedrock.spec.PoolSpec, databases: list=[]):
-    return bedrock.spec.ProviderSpec(name=f'hepnos_{provider_id}',
-                                     type='yokan', pool=pool,
-                                     provider_id=provider_id,
-                                     config={'databases': databases})
+def gen_provider_spec(provider_id: int=0, pool=mochi.bedrock.spec.PoolSpec, databases: list=[]):
+    return mochi.bedrock.spec.ProviderSpec(name=f'hepnos_{provider_id}',
+                                           type='yokan', pool=pool,
+                                           provider_id=provider_id,
+                                           config={'databases': databases})
 
 def gen_config(output: str='',
                address: str='na+sm',
@@ -58,13 +58,13 @@ def gen_config(output: str='',
     if num_providers < num_rpc_pools:
         raise ValueError('Number of RPC pools exceeds the number of providers')
     # define the rool ProcSpec
-    proc_spec = bedrock.spec.ProcSpec(margo=address)
+    proc_spec = mochi.bedrock.spec.ProcSpec(margo=address)
     # add the yokan library
     proc_spec.libraries['yokan'] = 'libyokan-bedrock-module.so'
     # add SSG info
     proc_spec.ssg.add(name='hepnos', bootstrap='mpi', group_file=ssg_group_file,
                       pool=proc_spec.margo.argobots.pools[0],
-                      swim=bedrock.spec.SwimSpec(disabled=True))
+                      swim=mochi.bedrock.spec.SwimSpec(disabled=True))
     # add the progress pool if needed
     if use_progress_xstream:
         progress_pool = proc_spec.margo.argobots.pools.add(
@@ -72,7 +72,7 @@ def gen_config(output: str='',
         proc_spec.margo.progress_pool = progress_pool
         progress_es = proc_spec.margo.argobots.xstreams.add(
             name='__progress__',
-            scheduler=bedrock.spec.SchedulerSpec(type='basic_wait', pools=[progress_pool]))
+            scheduler=mochi.bedrock.spec.SchedulerSpec(type='basic_wait', pools=[progress_pool]))
     # add rpc pools
     rpc_pools = []
     for i in range(num_rpc_pools):
@@ -85,7 +85,7 @@ def gen_config(output: str='',
     for i in range(num_rpc_xstreams):
         proc_spec.margo.argobots.xstreams.add(
             name=f'__rpc_{i}__',
-            scheduler=bedrock.spec.SchedulerSpec(type='basic_wait', pools=[rpc_pools[i%len(rpc_pools)]]))
+            scheduler=mochi.bedrock.spec.SchedulerSpec(type='basic_wait', pools=[rpc_pools[i%len(rpc_pools)]]))
     # create databases
     dbs = []
     for i in range(num_dataset_databases):
